d5152a14974787470967c85eecdb73c2262d9f77
arman <mdarmanhossen404@gmail.com>
2025-05-06 23:41:29 +0600

    Skyline_backend

--- Hotel_Booking/settings.py
+++ Hotel_Booking/settings.py
@@ -28,8 +28,8 @@ ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
     'corsheaders',
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,10 +45,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -61,6 +61,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5501",
+]
+
+
 
 CORS_ALLOW_METHODS = [
     "GET",
