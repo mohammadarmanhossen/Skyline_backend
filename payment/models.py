4635db1cedd51ef49bba2b011cdc93a9810287f8
arman <mdarmanhossen404@gmail.com>
2025-05-28 09:16:27 +0600

    Skyline_backend

--- payment/models.py
+++ payment/models.py
@@ -3,15 +3,13 @@ from django.contrib.auth.models import User
 from hotels.models import Booked
 from hotels.models import Hotel
 
-
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    address = models.TextField()
+    address= models.TextField()
     zip_code = models.CharField(max_length=10)
-    booked = models.ForeignKey(Booked, on_delete=models.CASCADE, related_name="orders", null=True, blank=True)
-    
+
 class Checkout(models.Model):
     booked = models.ForeignKey(Booked, on_delete=models.CASCADE, related_name="checkouts", default=1)  
     is_paid = models.BooleanField(default=False)
