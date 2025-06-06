5cf9e5906656883e4d4fccac31c7bef6da4921e4
arman <mdarmanhossen404@gmail.com>
2025-06-06 07:47:46 +0600

    Skyline_backend

--- payment/models.py
+++ payment/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from hotels.models import Booked
-from hotels.models import Hotel
+from hotels.models import Booked
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    booked = models.OneToOneField(Booked, on_delete=models.CASCADE, related_name="order", null=True, blank=True)
+
     name = models.CharField(max_length=255)
     email = models.EmailField()
     address= models.TextField()
     zip_code = models.CharField(max_length=10)
+
 
 class Checkout(models.Model):
     booked = models.ForeignKey(Booked, on_delete=models.CASCADE, related_name="checkouts", default=1)  
--- payment/serializer.py
+++ payment/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Checkout, Order
+from .models import Checkout,Order
 
 
 
